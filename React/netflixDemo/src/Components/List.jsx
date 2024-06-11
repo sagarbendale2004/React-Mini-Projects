b37bb79971cf4b054552bd036131fb1f50e882bf
Sagar Bendale <99851764+sagarbendale2004@users.noreply.github.com>
2024-06-11 14:15:20 +0530

    Update List.jsx

--- React/netflixDemo/src/Components/List.jsx
+++ React/netflixDemo/src/Components/List.jsx
@@ -3,11 +3,13 @@ import Li from "../Components/Li";
 
 function List() {
   return (
-    <ul className="flex gap-8 justify-center mb-8">
-      {seriesData.map((item) => {
-        return <Li key={item.id} items={item} />;
-      })}
-    </ul>
+    <div className="w-[96rem] mx-auto">
+      <ul className="grid grid-cols-4 mb-4 gap-4">
+        {seriesData.map((item) => {
+          return <Li key={item.id} items={item} />;
+        })}
+      </ul>
+    </div>
   );
 }
 
