0829eba3eb18c7e449f1c30364bc5262cee3b0fc
Sagar Bendale <99851764+sagarbendale2004@users.noreply.github.com>
2024-06-11 14:14:49 +0530

    Update Li.jsx

--- React/netflixDemo/src/Components/Li.jsx
+++ React/netflixDemo/src/Components/Li.jsx
@@ -1,12 +1,19 @@
 // eslint-disable-next-line react/prop-types
-function Li({ items: { url, title, genre, rating, description } }) {
+function Li({ items: { url, title, genre, rating, description, watchUrl } }) {
+  function handleClick() {
+    window.open(watchUrl, "_blank", "noopener,noreferrer");
+  }
+
   return (
-    <li className="w-[320px] max-h-min bg-gray-800 rounded-2xl border-2 border-gray-600 p-3">
-      <img className="h-48 w-80 rounded-2xl" src={url} alt="new image" />
+    <li className="w-[350px] max-h-min bg-gray-800 rounded-xl border-2 border-gray-600 p-3">
+      <img className="h-48 w-full rounded-2xl" src={url} alt="new image" />
       <h2 className="py-2 text-xl font-sans">Title : {title}</h2>
       <h2 className="pb-2 text-lg">Genre : {genre}</h2>
       <h2 className="text-lg">Rating : {rating}</h2>
       <p className="text-lg py-2 text-wrap">Description : {description}</p>
+      <button className="py-2 w-full bg-slate-700" onClick={handleClick}>
+        Watch Now
+      </button>
     </li>
   );
 }
