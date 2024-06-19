8715db1a8050f5a0dfb0d0206a403443e11380c6
Sagar Bendale <99851764+sagarbendale2004@users.noreply.github.com>
2024-06-19 17:17:53 +0530

    Update List.jsx

--- React/netflixDemo/src/Components/List.jsx
+++ React/netflixDemo/src/Components/List.jsx
@@ -4,6 +4,9 @@ import Li from "../Components/Li";
 function List() {
   return (
     <div className="w-[96rem] mx-auto">
+      <h1 className="text-3xl font-mono py-12 text-gray-400">
+        Trending Movies :
+      </h1>
       <ul className="grid grid-cols-4 mb-4 gap-4">
         {seriesData.map((item) => {
           return <Li key={item.id} items={item} />;
@@ -14,3 +17,4 @@ function List() {
 }
 
 export default List;
+
